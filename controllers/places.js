daae50650b327240928580e71e3d25e29b79a2c8
Nalinaka <nalinaka@gmail.com>
2023-06-04 19:46:14 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const places = require ('../models/places.js')
+
+router.get('/', (req, res) => {
+res.render('places/index', { places })
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
 
 // GET /places
-router.get('/', (req, res) => {
+
   let places = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
@@ -21,12 +44,7 @@ router.get('/', (req, res) => {
     console.log(req.body)
     res.send('POST /places')
   })
-  
-      
-    res.render('places/index', { places })
 
   })
-
-
   
 module.exports = router
