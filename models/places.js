63e305062496606c5d93719e35c16cc27e2f9243
Nalinaka <nalinaka@gmail.com>
2023-06-12 20:16:49 -0700

    Completed

--- models/places.js
+++ models/places.js
@@ -1,44 +1,25 @@
 const mongoose = require('mongoose')
-const router = require('express').Router()
-const db = require('../models')
-
-
-router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-      res.render('places/index', { places })
-    })
-    .catch(err => {
-      console.log(err) 
-      res.render('error404')
-    })
-})
-
-// CREATE
-router.post('/', (req, res) => {
-    db.Place.create(req.body)
-    .then(() => {
-        res.redirect('/places')
-    })
-    .catch(err => {
-        console.log('err', err)
-        res.render('error404')
-    })
-  })
-  
-  
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: String,
+  pic: { type: String, default: 'http://placekitten.com/350/350' },
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
-  founded: Number
+  founded: {
+    type: Number,
+    min: [1673, 'Surely not that old?!'],
+    max: [new Date().getFullYear(), 'Hey, this year is in the future!']
+  },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 })
 
-module.exports = mongoose.model('Place', placeSchema)
 
+//placeSchema.methods.showEstablished = function() {
+ // return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+// }
+
+module.exports = mongoose.model('Place', placeSchema)
 
 
 
